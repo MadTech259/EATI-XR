6956fe71341d045befb840d64d2566fcd50395f2
E.A.T.I <eati@labs.cs.uns.edu.ar>
2020-02-17 11:42:56 -0300

    first day

---
+++ Assets/BoxLauncherBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxLauncherBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float Xdirection;
+    [SerializeField] private float Ydirection;
+    [SerializeField] private float Zdirection;
+    private float holdTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Stationary)
+            {
+                holdTime += Time.deltaTime;
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                var currentP = Instantiate(projectile, transform.position, transform.rotation);
+                Vector3 force = Vector3.forward* 500;
+                Debug.Log(transform.forward);
+
+                force.x += Xdirection;
+                force.y += Ydirection;
+                force.z += Zdirection;
+                force *= (holdTime * 50);
+                currentP.GetComponent<Rigidbody>().AddForce(force);
+                holdTime = 0;
+
+            }
+
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            holdTime += Time.deltaTime;
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            var currentP = Instantiate(projectile, transform.position, transform.rotation);
+            Vector3 force =  transform.forward;
+            Debug.Log(transform.forward);
+
+            force.x += Xdirection;
+            force.y += Ydirection;
+            force.z += Zdirection;
+            force *= (holdTime * 50);
+            currentP.GetComponent<Rigidbody>().AddForce(force);
+            holdTime = 0;
+
+        }
+    }
+}
---
+++ Assets/CameraBeahaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBeahaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Input.isGyroAvailable)
+        {
+            Input.gyro.enabled = true;
+        }
+    }
+
+
+    
+        public float speedH = 2.0f;
+        public float speedV = 2.0f;
+
+        private float yaw = 0.0f;
+        private float pitch = 0.0f;
+
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        if (Input.gyro.enabled)
+        {
+            transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+        }
+    }
+
+
+}
